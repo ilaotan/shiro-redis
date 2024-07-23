8588bc060ff5e3037968fbc242f8600e34fce5f5
不想当厨子的老谭 <ilaotan@users.noreply.github.com>
2024-07-23 17:52:12 +0800

    Update WorkAloneRedisManager.java

--- src/main/java/org/crazycake/shiro/common/WorkAloneRedisManager.java
+++ src/main/java/org/crazycake/shiro/common/WorkAloneRedisManager.java
@@ -3,8 +3,8 @@ package org.crazycake.shiro.common;
 import org.crazycake.shiro.IRedisManager;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.ScanParams;
-import redis.clients.jedis.ScanResult;
+import redis.clients.jedis.params.ScanParams;
+import redis.clients.jedis.resps.ScanResult;
 
 import java.util.HashSet;
 import java.util.List;
